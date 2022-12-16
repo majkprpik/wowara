1f1120428634459e86a3524e1d299ad352e27834
majkprpik <majkprpik@users.noreply.github.com>
2022-12-16 14:59:04 +0100

    admin update

--- ClientApp/src/app/admin/admin.component.ts
+++ ClientApp/src/app/admin/admin.component.ts
@@ -5,15 +5,21 @@ import { AdminService } from '../services/admin.service';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent implements OnInit {
   families: Family[] = [];
   creatures: Creature[] = [];
   allCreatures: Creature[] = [];
-  newCreature: NewCreature  = {id: 0, displayId: 0, name: '', imageUrl: '', attack: 0};
+  newCreature: NewCreature = {
+    id: 0,
+    displayId: 0,
+    name: '',
+    imageUrl: '',
+    attack: 0,
+  };
 
-  constructor(private adminService: AdminService) { }
+  constructor(private adminService: AdminService) {}
 
   ngOnInit(): void {
     this.adminService.families.subscribe((data: any) => {
@@ -21,7 +27,7 @@ export class AdminComponent implements OnInit {
     });
     this.adminService.creatures.subscribe((data: any) => {
       this.creatures = data;
-    }); 
+    });
     this.adminService.allCreatures.subscribe((data: any) => {
       this.allCreatures = data;
     });
@@ -32,23 +38,38 @@ export class AdminComponent implements OnInit {
   }
 
   selectCreature(creatureId: number) {
-    let tempCreature = this.creatures.find(c => c.id === creatureId);
+    let tempCreature = this.creatures.find((c) => c.id === creatureId);
 
     this.newCreature.id = tempCreature?.id ? tempCreature.id : 0;
-    this.newCreature.displayId = tempCreature?.displayId ? tempCreature.displayId : 0;
+    this.newCreature.displayId = tempCreature?.displayId
+      ? tempCreature.displayId
+      : 0;
     this.newCreature.name = tempCreature?.name ? tempCreature.name : '';
-    this.newCreature.imageUrl = tempCreature?.imageUrl ? tempCreature.imageUrl : '';
+    this.newCreature.imageUrl = tempCreature?.imageUrl
+      ? tempCreature.imageUrl
+      : '';
     this.newCreature.attack = tempCreature?.attack ? tempCreature.attack : 0;
   }
 
   addNewCreature() {
+    if (this.newCreature.attack == 0 || this.newCreature.name == '') {
+      alert('Please fill in all fields!');
+      return;
+    }
     this.adminService.addNewCreature(this.newCreature).subscribe(
       (data: any) => {
         alert('New creature added!');
-        this.newCreature = {id: 0, displayId: 0, name: '', imageUrl: '', attack: 0};
+        this.newCreature = {
+          id: 0,
+          displayId: 0,
+          name: '',
+          imageUrl: '',
+          attack: 0,
+        };
+        this.adminService.getAllCreatures();
       },
       (error: any) => {
-        alert("Attack " + this.newCreature.attack + " is already taken!");
+        alert('Attack ' + this.newCreature.attack + ' is already taken!');
       }
     );
   }
