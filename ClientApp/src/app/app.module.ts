bac4b92f0645dbab86a8b92b383911d01ceb8c04
majkprpik <majkprpik@users.noreply.github.com>
2022-12-16 12:06:54 +0100

    app server returned

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,10 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CreatureComponent } from './creature/creature/creature.component';
 
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
